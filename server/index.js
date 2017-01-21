3765ce41eaa537eb8c9fae2a19bdb4e5749dfc33
Ke Zhu <kzhu@us.ibm.com>
2017-01-21 00:26:43 -0500

    generate JWT token

--- server/index.js
+++ server/index.js
@@ -18,6 +18,19 @@ module.exports = function(app) {
 
   // Log proxy requests
   var morgan  = require('morgan');
+
+  var jwt = require('jsonwebtoken'),
+    privateKey = process.env['JWT_PRICATE_KEY'],
+    getJWTToken = () => {
+      let payload = {
+        iss: 'Travis CI, GmbH',
+        exp: Date.now() + 7200
+      };
+
+      return jwt.sign(payload, privateKey, { algorithm: 'RS256' });
+    }
+
+
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -27,6 +40,8 @@ module.exports = function(app) {
   // this is reimplementation of waiter/lib/travis/web/set_token.rb
   app.use(function(req, res, next) {
     var token = req.body['token'];
+    var jwtToken = getJWTToken();
+
     if(req.method == 'POST' && token && token.match(/^[a-zA-Z\-_\d]+$/)) {
       var storage = req.body['storage'];
       if(storage !== 'localStorage') {
@@ -38,6 +53,7 @@ module.exports = function(app) {
         <script>
           var storage = ${storage};
           storage.setItem('travis.token', '${token}');
+          storage.setItem('travis.jwt', '${jwtToken}');
           storage.setItem('travis.user', ${user});
           storage.setItem('travis.become', true);
           window.location = '${req.path}';
